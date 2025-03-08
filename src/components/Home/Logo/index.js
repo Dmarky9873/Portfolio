378ebd826168990baf86a7e3abae13c90347a6ed
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-03-08 14:27:11 -0500

    fixed gsap error

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import gsap from 'gsap'
+import DrawSVGPlugin from 'gsap/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-d.png'
 import './index.scss'
 
