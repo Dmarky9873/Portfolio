ce5e254e6a260832c7b9d29e648870a979752474
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-03-20 15:22:09 -0400

    Fixes and added resume to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import './index.scss'
+import Resume from '../../assets/files/resume-daniel-markusson.pdf'
 
 const Footer = () => {
     return (
         <footer>
             <div className='footer-text'>
-                You made it to the bottom. <a>Click me.</a>
+                You made it to the bottom. <Link to={Resume} target="_blank" download='resume-daniel-markusson.pdf'>Download my resume</Link>
             </div>
         </footer>
     )
