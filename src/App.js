9288572b572816384741497dfe820cdbf0f6af68
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2025-12-23 18:47:43 -0500

    added page for first rbc internship

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contact from './components/pages/Contact';
 import Portfolio from './components/pages/Portfolio';
 import PageNotFound from './components/pages/PageNotFound';
 import AdvancedRobotics from './components/pages/Portfolio/pages/AdvancedRobotics';
+import FirstRBCInternship from './components/pages/Portfolio/pages/FirstRBCInternship';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             path="portfolio/advanced-robotics"
             element={<AdvancedRobotics />}
           />
+          <Route path="portfolio/first-rbc-internship" element={<FirstRBCInternship />} />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -42,7 +42,7 @@ const Portfolio = () => {
           imageAlt="Robotics in Bordeaux with Luke, Hayden, and Mr. Koaich"
           link="/portfolio/advanced-robotics"
           title="Advanced Robotics Club"
-          date="2022-2025"
+          date="2022 — 2025"
         >
           How did three highschoolers end up representing their country on the
           international stage? I don't know either. But somehow, against all
@@ -55,7 +55,7 @@ const Portfolio = () => {
         <TitleImageText
           image={STLGroupPhoto}
           imageAlt="Group photo with members of the RBC Summer Tech Labs"
-          link="/portfolio/rbc-internship"
+          link="/portfolio/first-rbc-internship"
           title="RBC Summer Tech Labs First Internship"
           date="2024"
         >
