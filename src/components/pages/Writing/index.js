4f3a9142afaf5f3c4da495476faf2ea679968ae8
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2026-04-01 01:12:29 -0400

    arst

--- src/components/pages/Writing/index.js
+++ src/components/pages/Writing/index.js
@@ -6,9 +6,11 @@ import TitleImageText from '../../elements/TitleImageText';
 import Footer from '../../elements/Footer';
 import LoadingAnimation from '../../elements/LoadingAnimation';
 import { writingItems } from '../../../data/writing';
+import fakeNewsPaperPreview from '../../../assets/images/writing/a-minimal-approach-to-fake-news-detection.png';
 
 const Writing = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+  const [firstWritingItem, ...remainingWritingItems] = writingItems;
 
   useEffect(() => {
     const timeoutId = window.setTimeout(() => {
@@ -34,7 +36,29 @@ const Writing = () => {
           Here are some papers I&apos;ve written you may enjoy.
         </Paragraph>
 
-        {writingItems.map((item) => (
+        <TitleImageText
+          image={firstWritingItem.previewImage}
+          imageAlt={firstWritingItem.previewAlt}
+          link={`/writing/${firstWritingItem.slug}`}
+          title={firstWritingItem.title}
+          date={firstWritingItem.meta}
+        >
+          {firstWritingItem.listDescription}
+        </TitleImageText>
+
+        <TitleImageText
+          image={fakeNewsPaperPreview}
+          imageAlt="First page preview of A Minimal Approach to Fake News Detection."
+          link="/portfolio/a-minimal-approach-to-fake-news-detection"
+          title="A Minimal Approach to Fake News Detection"
+          date="2024 / Research Paper"
+        >
+          Over six months, I published a research paper on machine learning
+          approaches to fake news detection. It reviews existing approaches and
+          presents a novel method.
+        </TitleImageText>
+
+        {remainingWritingItems.map((item) => (
           <TitleImageText
             key={item.slug}
             image={item.previewImage}
